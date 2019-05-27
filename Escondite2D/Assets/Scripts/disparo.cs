efaceb1f9067e7b43c4ef7c71809ac7005de91f7
Antonio <reytono@gmail.com>
2019-05-27 12:54:14 -0600

    avances

---
+++ Escondite2D/Assets/Scripts/disparo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour
+{
+    public float ballSpeed;
+    private Rigidbody2D theRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Escondite2D/Assets/Scripts/personaje2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje2 : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+    public KeyCode shot;
+
+    private Rigidbody2D theRB;
+
+    public GameObject snowBall;
+    public Transform throwPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }
+
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        } else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if (Input.GetKeyDown(jump))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.y, jumpForce);
+        }
+
+        if (Input.GetKeyDown(shot))
+        {
+            Instantiate(snowBall, throwPoint.position, throwPoint.rotation);
+        }
+
+    }
+}
